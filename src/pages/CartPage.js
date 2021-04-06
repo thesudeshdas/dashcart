3785597c5a518830be5fa162874cc4e9b23a2d6c
thesudeshdas <sudeshkumardas7@gmail.com>
2021-04-06 13:30:25 +0530

    refactor: eslint config

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -4,15 +4,14 @@ import { CartItemCard } from '../components/index'
 function CartPage() {
   const {
     state: { cartList },
-    dispatch: cartDispatch,
   } = useCartContext()
 
   return (
     <>
       <h1>Shopping Cart</h1>
       <div>
-        {cartList.map((product, index) => (
-          <CartItemCard product={product} />
+        {cartList.map((product) => (
+          <CartItemCard key={product.id} product={product} />
         ))}
       </div>
     </>

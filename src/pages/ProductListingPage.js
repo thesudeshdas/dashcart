47a3a8a5175eadac2991e40a0899a5d41f1a25e0
thesudeshdas <sudeshkumardas7@gmail.com>
2021-04-06 13:32:54 +0530

    refactor: cleaned code

--- src/pages/ProductListingPage.js
+++ src/pages/ProductListingPage.js
@@ -1,37 +1,14 @@
-import { useState } from 'react'
 import productData from '../database/productData'
-import * as icons from '../assets/icons'
-import { useCartContext } from '../contexts/CartContext'
-import { useWishlistContext } from '../contexts/WishlistContext'
 import { ProductCard } from '../components'
 
 function ProductListingPage() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [productList, setProductList] = useState(productData)
-  const {
-    state: { cartList },
-    dispatch: cartDispatch,
-  } = useCartContext()
-
-  const {
-    state: { wishlist },
-    dispatch: wishlistDispatch,
-  } = useWishlistContext()
-
-  const caalFunc = () => <h1>Hello</h1>
+  const productList = productData
 
   return (
     <div>
-      {isLoading && (
-        <h1>
-          Finding the best stuff for you
-          <icons.IonLoadA />
-        </h1>
-      )}
-
       <div className="container-card">
-        {productList.map((product, index) => (
-          <ProductCard product={product} />
+        {productList.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
--- src/pages/WishlistPage.js
+++ src/pages/WishlistPage.js
@@ -1,21 +1,17 @@
 import { WishlistItemCard } from '../components'
-import { useCartContext } from '../contexts/CartContext'
 import { useWishlistContext } from '../contexts/WishlistContext'
 
 function WishlistPage() {
   const {
     state: { wishlist },
-    dispatch: wishlistDispatch,
   } = useWishlistContext()
-
-  const { dispatch: cartDispatch } = useCartContext()
 
   return (
     <>
       <h1>This is Wishlist</h1>
       <div className="container-wishlist-item">
         {wishlist.map((product) => (
-          <WishlistItemCard product={product} />
+          <WishlistItemCard key={product.id} product={product} />
         ))}
       </div>
     </>
